6a892fee8bb1921f76b7e55afad853f2269c67f1
peach mercader <peachmercader@gmail.com>
2023-03-04 11:04:02 +1100

    immunisations

--- src/Immunisations.js
+++ src/Immunisations.js
@@ -1,54 +1,97 @@
 import React, { useState, useEffect } from 'react';
 import { db } from "./firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 
 function ImmunisationsData() {
     const [immunisationData, setImmunisationData] = useState(null);
 
-    const userId = "gGO7OvCLuHRrIwaG3q9loH8KV443";
-    const childId = "x7XGtVpnPmC9hX79iUkJ";
+    // const userId = "gGO7OvCLuHRrIwaG3q9loH8KV443";
+    // const childId = "x7XGtVpnPmC9hX79iUkJ";
 
     useEffect(() => {
-        const fetchAllImmunisationData = async () => {
-            try {
-                const immunisationsRef = doc(db, `users/${userId}/children/${childId}/baby_details/immunisations`);
-                const immunisationsSnapshot = await getDoc(immunisationsRef);
-                const immunisationsData = immunisationsSnapshot.data();
-                const allImmunisationData = {};
+        const auth = getAuth();
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const fetchBabyDetails = async () => {
+                    try {
+                        const userId = user.uid;
+                        const childrenRef = collection(db, `users/${userId}/children`);
+                        const querySnapshot = await getDocs(childrenRef);
 
-                for (const age in immunisationsData) {
-                    const vaccines = immunisationsData[age];
-                    for (const vaccine in vaccines) {
-                        const immunisationId = Object.keys(vaccines).find((key) => vaccines[key].name === vaccine);
-                        const immunisationRef = doc(db, `users/${userId}/children/${childId}/baby_details/immunisations/${age}/${immunisationId}`);
-                        const immunisationSnapshot = await getDoc(immunisationRef);
-                        const immunisationData = immunisationSnapshot.data();
-                        allImmunisationData[age] = allImmunisationData[age] || {};
-                        allImmunisationData[age][vaccine] = immunisationData;
-
-                        console.log(immunisationId);
+                        await Promise.all(querySnapshot.docs.map(async (childDoc) => {
+                            const childId = childDoc.id;
+                            const babyDetailsRef = doc(db, `users/${userId}/children/${childId}/baby_details/birth_details`);
+                            const babyDetailsSnapshot = await getDoc(babyDetailsRef);
+                            const babyDetails = babyDetailsSnapshot.data();
+                            if (babyDetails) {
+                                console.log(babyDetails);
+                                // setBabyName(babyDetails.baby_name);
+                                // setDateOfBirth(babyDetails.birth_date);
+                                // setBirthDetails({
+                                //     hospital: babyDetails.hospital,
+                                //     birth_date: babyDetails.birth_date,
+                                //     weight: babyDetails.weight,
+                                //     length: babyDetails.length,
+                                //     head_circumference: babyDetails.head_circumference,
+                                //     baby_name: babyDetails.baby_name
+                                
+                            }
+                        }));
+                    } catch (error) {
+                        console.log("Error fetching baby details:", error);
                     }
-                }
-
-                setImmunisationData(allImmunisationData);
-            } catch (error) {
-                console.log("Error fetching immunisation data:", error);
+                };
+                fetchBabyDetails();
             }
-        };
-
-        fetchAllImmunisationData();
+        });
+        return () => unsubscribe();
     }, []);
 
+    // /users/${userId}/children/{childId}/baby_details/immunisations/birth
+    // users/${userId}/children/${childId}/baby_details/birth_details
+    // useEffect(() => {
+    //     const fetchAllImmunisationData = async () => {
+    //         console.log('im here---------')
+    //         try {
+    //             const immunisationsRef = doc(db, `users/${userId}/children/${childId}/baby_details/immunisations`);
+    //             const immunisationsSnapshot = await getDoc(immunisationsRef);
+    //             const immunisationsData = immunisationsSnapshot.data();
+    //             const allImmunisationData = {};
+    //             console.log(immunisationsData)
+    //             for (const age in immunisationsData) {
+    //                 const vaccines = immunisationsData[age];
+    //                 for (const vaccine in vaccines) {
+    //                     const immunisationId = Object.keys(vaccines).find((key) => vaccines[key].name === vaccine);
+    //                     const immunisationRef = doc(db, `users/${userId}/children/${childId}/baby_details/immunisations/${age}/${immunisationId}`);
+    //                     const immunisationSnapshot = await getDoc(immunisationRef);
+    //                     const immunisationData = immunisationSnapshot.data();
+    //                     allImmunisationData[age] = allImmunisationData[age] || {};
+    //                     allImmunisationData[age][vaccine] = immunisationData;
 
-    console.log('Immunisation Data:', immunisationData);
+    //                     console.log(immunisationId);
+    //                 }
+    //             }
 
-    if (!immunisationData) {
-        return <p>Loading...</p>;
-    }
+    //             setImmunisationData(allImmunisationData);
+    //         } catch (error) {
+    //             console.log("Error fetching immunisation data:", error);
+    //         }
+    //     };
+
+    //     fetchAllImmunisationData();
+    // }, []);
+
+
+    // console.log('Immunisation Data:', immunisationData);
+
+    // if (!immunisationData) {
+    //     return <p>Loading...</p>;
+    // }
 
     return (
         <div>
-            {Object.entries(immunisationData).map(([age, vaccines]) => (
+            {/* {Object.entries(babyDetails).map(([age, vaccines]) => (
                 <div key={age}>
                     <h3>{age}</h3>
                     {Object.entries(vaccines).map(([vaccine, data]) => (
@@ -66,7 +109,7 @@ function ImmunisationsData() {
                         </div>
                     ))}
                 </div>
-            ))}
+            ))} */}
         </div>
     );
 }
